dbe9347c394fe7494bb1360663e59039c7452642
ledeep <smarreddy1@gmail.com>
2017-05-21 14:17:37 -0700

    Normalize model data for name, email address, and phone number before saving to database

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,21 @@
 class Contact < ActiveRecord::Base
+  before_save :capitalize_first_name, :capitalize_last_name, :downcase_email_address, :normalize_phone_number
+
+  private
+
+  def capitalize_first_name
+    first_name.capitalize!
+  end
+
+  def capitalize_last_name
+    last_name.capitalize!
+  end
+
+  def downcase_email_address
+    email_address.downcase!
+  end
+
+  def normalize_phone_number
+    phone_number.gsub!(/\s|-|\.|\(|\)/, "").downcase!
+  end
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,33 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:contact) { FactoryGirl.create(:contact, first_name: "james", last_name: "BoND", email_address: "Jbond@SPY.COM", phone_number: "(724)196-9470 X998", company_name: "MI6")}
+
+  context "normalization" do
+    it "capitalizes the first name" do
+      expect(contact.first_name).to eq("James")
+    end
+
+    it "capitalizes the last name" do
+      expect(contact.last_name).to eq("Bond")
+    end
+
+    it "downcases the email address" do
+      expect(contact.email_address).to eq("jbond@spy.com")
+    end
+
+    it "normalizes a phone number with an extension" do
+      expect(contact.phone_number).to eq("7241969470x998")
+    end
+
+    it "normalizes a phone number without an extension" do
+      contact.update_attribute(:phone_number, "(724)196-9470")
+      expect(contact.phone_number).to eq("7241969470")
+    end
+
+    it "normalizes a phone number without international prefix" do
+      contact.update_attribute(:phone_number, "1-(724)196-9470")
+      expect(contact.phone_number).to eq("17241969470")
+    end
+  end
 end
