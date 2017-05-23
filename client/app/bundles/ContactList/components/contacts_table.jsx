316ef0a3e61829c0b83dc04c08ecb52e3137f405
ledeep <smarreddy1@gmail.com>
2017-05-23 00:09:05 -0700

    Update contacts table to filter and sort email addresses

--- client/app/bundles/ContactList/components/contacts_table.jsx
+++ client/app/bundles/ContactList/components/contacts_table.jsx
@@ -1,42 +1,51 @@
 import React from 'react';
+var ReactBSTable = require('react-bootstrap-table');
+var BootstrapTable = ReactBSTable.BootstrapTable;
+var TableHeaderColumn = ReactBSTable.TableHeaderColumn;
 
 export default class ContactsTable extends React.Component {
   handleDelete(id) {
     this.props.handleDelete(id);
   };
 
+  showDotComEmailAddresses = () => {
+    this.refs.table.handleFilterData({ email_address: '.com' });
+  };
+
+  showAllEmailAddresses = () => {
+    this.refs.table.handleFilterData({ email_address: '' });
+  };
+
   render() {
-    var tableStyle = {
-      width: '100%',
-      textAlign: "left"
+    function onAfterDeleteRow() {
+      alert('Contact has been succesfully deleted');
     };
-    var contacts = this.props.contacts.map((contact) => {
-      return (
-        <tr key={contact.id}>
-          <td>{contact.first_name}</td>
-          <td>{contact.last_name}</td>
-          <td>{contact.email_address}</td>
-          <td>{contact.phone_number}</td>
-          <td>{contact.company_name}</td>
-          <td><button onClick={this.handleDelete.bind(this, contact.id)} >Delete</button></td>
-        </tr>
-      )
-    });
+
+    const options = {
+      afterDeleteRow: onAfterDeleteRow  // A hook for after droping rows.
+    };
+
+    // If you want to enable deleteRow, you must enable row selection also.
+    const selectRowProp = {
+      mode: 'checkbox'
+    };
+
+    const buttonStyle = {
+      margin: '10px'
+    }
 
     return (
       <div>
-        <table style={tableStyle}>
-          <tbody>
-            <tr>
-              <th>First Name </th>
-              <th>Last Name </th>
-              <th>Email Address </th>
-              <th>Phone Number </th>
-              <th>Company Name </th>
-            </tr>
-            {contacts}
-          </tbody>
-        </table>
+        <button style={buttonStyle} onClick={ this.showDotComEmailAddresses } className='btn btn-success'>Show contacts with '.com' email address</button>
+        <button onClick={ this.showAllEmailAddresses } className='btn btn-success'>Show all contacts</button>
+        <BootstrapTable ref='table' data={ this.props.contacts } deleteRow={ true } selectRow={ selectRowProp } options={ options }>
+          <TableHeaderColumn width='30px' dataField='id' isKey={ true } hidden={ true }>id</TableHeaderColumn>
+          <TableHeaderColumn width='90px' dataField='first_name' >First Name</TableHeaderColumn>
+          <TableHeaderColumn width='98px' dataField='last_name' >Last Name</TableHeaderColumn>
+          <TableHeaderColumn width='302px' dataField='email_address' dataSort={ true }>Email Address</TableHeaderColumn>
+          <TableHeaderColumn width='157px' dataField='phone_number' >Phone Number</TableHeaderColumn>
+          <TableHeaderColumn width='282px' dataField='company_name' >Company Name</TableHeaderColumn>
+        </BootstrapTable>
       </div>
     )
   }
